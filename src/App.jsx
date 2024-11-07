b65c9a82b1aa3bae02a71e8ebe15fb30b8641b56
Nicolas Baracaldo <nicolasbquintero@gmail.com>
2024-11-07 13:38:59 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './main.scss'
+
+function App() {
+  return (
+    <>
+      <div className='test'>
+        <h1 className='test-title'>Evolusystem</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
